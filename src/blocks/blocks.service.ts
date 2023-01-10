ab296ba88d82372c1b51e08bd7ee2a3363d75bc6
Denis.S <denis.s@MacBook-Pro-3.local>
2023-01-10 09:29:33 +0200

    fix image url instead object

--- src/blocks/blocks.service.ts
+++ src/blocks/blocks.service.ts
@@ -3,13 +3,19 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { PageBlockRecord } from './interfaces/pageBlockRecord';
 import { PageBlocks, PageBlocksDocument } from './schemas/blocks';
+import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class BlocksService {
+  private readonly staticEndpoint: URL;
   constructor(
+    private readonly configService: ConfigService,
     @InjectModel(PageBlocks.name)
     private readonly pageBlocksModel: Model<PageBlocksDocument>,
-  ) {}
+  ) {
+    this.staticEndpoint = new URL(this.configService.get<string>('aws.s3.endpoint'));
+  }
 
   async list(page: number): Promise<PageBlockRecord[]> {
     const result = await this.pageBlocksModel
@@ -26,12 +32,22 @@ export class BlocksService {
     return blocks;
   }
 
-  private async toPageBlockRecord(payload: PageBlocksDocument): Promise<PageBlockRecord> {
+  private async toPageBlockRecord(payload): Promise<PageBlockRecord> {
+    const data = payload.data;
+    if (data.image) {
+      data.image = await this.getStaticUrl(payload._id.toString(), data.image);
+    }
     return {
       id: payload._id,
       title: payload.title,
       type: payload.type,
       data: payload.data,
     };
+  }
+
+  private async getStaticUrl(blockId: string, { filename, mimeType, contentLength }): Promise<string> {
+    const url = new URL(this.staticEndpoint);
+    url.pathname = join(blockId, filename);
+    return url.toString();
   }
 }
